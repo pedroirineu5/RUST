39c3161bc1b583e7ff1e6d91dd8267ff2ff3a20d
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2023-11-05 17:59:44 -0300

    gfdnsgndfsjk

--- crashcoursefcc/src/main.rs
+++ crashcoursefcc/src/main.rs
@@ -1,7 +1,7 @@
 fn main(){
 
-    let v1: u8 = 251_u16 + 8+u8;
-    let v2 = i8 ::checked_add(251, 8).unwrap();// uma forma mais segura
+    let v1: u16 = 251_u16 + 8;
+    let v2: i16 = i16::checked_add(251, 8).unwrap();// uma forma mais segura
 
     println!("{},{}",v1,v2);
-}
+} 
