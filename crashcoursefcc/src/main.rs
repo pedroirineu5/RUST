4e349e0d0f6ff06e10dd6c35b77a69d7c1f3fac3
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2023-11-05 17:41:51 -0300

    fgasdfsdamkl

--- crashcoursefcc/src/main.rs
+++ crashcoursefcc/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+
+    let v1: u8 = 251_u16 + 8+u8;
+    let v2 = i8 ::checked_add(251, 8).unwrap();// uma forma mais segura
+
+    println!("{},{}",v1,v2);
 }
